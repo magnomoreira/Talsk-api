2b766ac83919d69e1103b4fd0ed4fce03c04d63c
Magno Moreira <magnomoreira@yahoo.com>
2021-07-04 12:16:20 -0300

    Criando  contratos

---
+++ TalksApi/Models/Contratos/CostumerRequest.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace TalksApi.Models.Contratos
+{
+	public class CostumerRequest
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Document { get; set; }
+		public string DocumentType { get; set; }
+	}
+}
