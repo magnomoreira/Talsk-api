5040f743a7b5c46a2f6a64c7f33c392b0120882f
Magno Moreira <magnomoreira@yahoo.com>
2021-07-04 11:59:36 -0300

    Preparando  projeto e criando a classe controller

---
+++ TalksApi/Controllers/CostumerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TalksApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CostumerController : ControllerBase
+	{
+		[HttpPost]
+		public IActionResult Post()
+		{
+			return Ok();
+		}
+	}
+}
