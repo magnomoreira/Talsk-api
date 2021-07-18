50e135683164446ffba89d98f2812eef9a083664
Magno Moreira <magnomoreira@yaho.com>
2021-07-18 19:19:40 -0300

    Criando Classe Context

---
+++ TalksApi/DataContext/TalksContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TalksApi.Entitie;
+
+namespace TalksApi.DataContext
+{
+	public class CustumerContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseMySql("Server=localhost;DataBase=users_data_base;Uid=root;Pwd=adimin123");
+
+		}
+
+	}
+}
